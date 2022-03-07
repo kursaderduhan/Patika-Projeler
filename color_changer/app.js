035e6355e0b8da437ecf8ffa41352f6f81ea06d4
kursaderduhan <kursaderduhan42@gmail.com>
2022-03-07 23:48:21 +0300

    proje

---
+++ color_changer/app.js
@@ -0,0 +1,9 @@
+function change(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.getElementById("container").style.backgroundColor= color;
+    return color;
+}
